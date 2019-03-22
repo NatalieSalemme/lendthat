630739da28f7bb1341b1c6efc461e28112e79dce
Natalie Salemme <NatalieSalemme@gmail.com>
2019-03-21 22:31:32 -0700

    setting up redux

--- services/passport.js
+++ services/passport.js
@@ -22,20 +22,19 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
+      proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //we already have a record with the given profile id
-          //done(error, user record)
-          done(null, existingUser);
-        } else {
-          //we dont have a user with this id
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        //we already have a record with the given profile id
+        //done(error, user record)
+        return done(null, existingUser);
+      }
+      //we dont have a user with this id
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
