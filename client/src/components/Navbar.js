861a0eae6c9d2931f23a67e15860189ea1e29763
Natalie Salemme <NatalieSalemme@gmail.com>
2019-03-22 01:19:51 -0700

    removing extra code from Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Payments from './Payments';
+// import Payments from './Payments';
 
 class Navbar extends Component {
   renderContent() {
@@ -16,9 +16,10 @@ class Navbar extends Component {
         );
       default:
         return [
-          <li key="1">
-            <Payments />
-          </li>,
+          // <li key="1">
+          //   <Payments />
+          // </li>,
+          // <li key="2">Credits: {this.props.auth.credits}</li>,
           <li key="3">
             <a href="/api/logout">Logout</a>
           </li>,
