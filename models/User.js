7754529539afdbaa965a6acfae37b5daf3d171cf
Natalie Salemme <NatalieSalemme@gmail.com>
2019-03-22 00:19:27 -0700

    adding middlewares and updating billingRoutes

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleId: String,
+  credits: { type: Number, default: 0 },
 });
 //Not using export due to possible issues during testing, require it into other files instead
 mongoose.model('users', userSchema);
